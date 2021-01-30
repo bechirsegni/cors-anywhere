03fb058cdb22db52801371b02daf2fee3b8d9728
bechir segni <bechirsegni@gmail.com>
2021-01-30 19:33:49 +0100

    add new header

--- server.js
+++ server.js
@@ -23,26 +23,7 @@ var cors_proxy = require('./lib/cors-anywhere');
 cors_proxy.createServer({
   originBlacklist: originBlacklist,
   originWhitelist: originWhitelist,
-  requireHeader: [
-    "x-gr-application":"iOS App",
-    "Authorization":"OAuth",
-    "realm": "",
-    "oauth_consumer_key":"T7rSxXydAsZg0dU3PJzFhw",
-    "oauth_token":"JhzxMxXLNkptCKxcXGQXg",
-    "oauth_signature_method":"HMAC-SHA1",
-    "oauth_signature":"nlK0nEpF8mhcPZnnW309xQT%2Fw0Q%3D",
-    "oauth_timestamp":"1611930497",
-    "oauth_nonce":"1815D290-FA94-4B83-9721-F43AB32DAC59",
-    "oauth_version":"1.0",
-    "Accept":"*/*",
-    "X_APPLE_DEVICE_MODEL":"iPhone",
-    "x-gr-os-version":"iOS 14.4",
-    "Accept-Language":"en-ES;q=1, ru-ES;q=0.9",
-    "X_APPLE_APP_VERSION":"702",
-    "x-gr-app-version":"702",
-    "x-gr-hw-model":"iPhone13,2",
-    "X_APPLE_SYSTEM_VERSION":"14.4"
-  ],
+  requireHeader: [],
   checkRateLimit: checkRateLimit,
   removeHeaders: [
     'cookie',
@@ -58,6 +39,27 @@ cors_proxy.createServer({
      'x-forwarded-proto',
      'x-forwarded-port',
   ],
+  setHeaders:[
+      {    "x-gr-application":"iOS App",
+          "Authorization":"OAuth",
+          "realm": "",
+          "oauth_consumer_key":"T7rSxXydAsZg0dU3PJzFhw",
+          "oauth_token":"JhzxMxXLNkptCKxcXGQXg",
+          "oauth_signature_method":"HMAC-SHA1",
+          "oauth_signature":"nlK0nEpF8mhcPZnnW309xQT%2Fw0Q%3D",
+          "oauth_timestamp":"1611930497",
+          "oauth_nonce":"1815D290-FA94-4B83-9721-F43AB32DAC59",
+          "oauth_version":"1.0",
+          "Accept":"*/*",
+          "X_APPLE_DEVICE_MODEL":"iPhone",
+          "x-gr-os-version":"iOS 14.4",
+          "Accept-Language":"en-ES;q=1, ru-ES;q=0.9",
+          "X_APPLE_APP_VERSION":"702",
+          "x-gr-app-version":"702",
+          "x-gr-hw-model":"iPhone13,2",
+          "X_APPLE_SYSTEM_VERSION":"14.4"
+        }
+  ]
   redirectSameOrigin: true,
   httpProxyOptions: {
     // Do not add X-Forwarded-For, etc. headers, because Heroku already adds it.
